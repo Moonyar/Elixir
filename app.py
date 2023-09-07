596e0d6c7135ba61a718591c47d3e2580ae7d5ab
Moonyar <mahyaresharafi@gmail.com>
2023-09-07 03:13:09 -0700

    Added edit functionality to CFLs but it is not fully working currently

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, jsonify, request
-from databse import load_cfl, add_cfl
+from datetime import datetime
+
+from flask import Flask, render_template, jsonify, request, url_for, redirect, flash
+from database import load_cfl, add_cfl, get_all_cfls, update_cfl
 
 app = Flask(__name__)
 
@@ -20,12 +22,52 @@ def show_cfl(id):
     return jsonify(cfl)
 
 
-@app.route("/cfl/submit", methods=['post'])
+@app.route("/cfl/submit", methods=['POST'])
 def submit_cfl():
     data = request.form
-    cfl_id=add_cfl(data)
-    cfl=load_cfl(cfl_id)
+    cfl_id = add_cfl(data)
+    cfl = load_cfl(cfl_id)
     return render_template('cfl_confirmed.html', cfl=cfl)
+
+
+@app.route("/cfl/edit/<int:cfl_id>", methods=['GET', 'POST'])
+def edit_cfl(cfl_id):
+    # Retrieve the record by ID
+    cfl = load_cfl(cfl_id)  # You'll need to define this function
+    if not cfl:
+        # Provide a flash message or some error to the user to indicate the problem.
+        # You can use Flask's flash function for this.
+        flash('CFL not found!', 'error')
+
+    if request.method == 'POST':
+        # Here you handle the updating of the record
+        update_cfl(cfl_id, request.form)
+        return redirect(url_for('/logout/view'))# or wherever you want to redirect after editing
+
+    return render_template('edit_cfl.html', cfl=cfl)
+
+
+@app.route("/logout/view", methods=['post'])
+def view_logout():
+    start_date = request.form.get('start_date')
+    end_date = request.form.get('end_date')
+
+    start_date = datetime.strptime(start_date, '%Y-%m-%dT%H:%M')
+    end_date = datetime.strptime(end_date, '%Y-%m-%dT%H:%M')
+
+    cfls = get_all_cfls(start_date, end_date)
+
+    return render_template('view_logout.html', cfls=cfls)
+
+
+@app.route("/logout")
+def logout():
+    return render_template('logout.html')
+
+
+@app.route("/summary")
+def summary():
+    return render_template('summary.html')
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -28,6 +28,7 @@ def load_cfl(id):
         else:
             return rows[0]._asdict()
 
+
 def add_cfl(data):
     with engine.connect() as conn:
         query = text(
@@ -41,3 +42,42 @@ def add_cfl(data):
                              "solution": data['solution']})
         result = conn.execute(text("SELECT LAST_INSERT_ID()"))
         return result.fetchone()[0]
+
+
+def get_all_cfls(start_date, end_date):
+    with engine.connect() as conn:
+        query = text("SELECT * FROM cfls WHERE updated_at BETWEEN :start AND :end")
+        records = conn.execute(query, {'start': start_date, 'end': end_date}).fetchall()
+
+    if len(records) == 0:
+        return None
+    else:
+        all = []
+        for row in records:
+            all.append(dict(row._mapping))
+
+    return all
+
+
+def update_cfl(cfl_id, data):
+    with engine.connect() as conn:
+        query = text("""
+            UPDATE cfls SET 
+            partner = :partner_name, 
+            leader = :leader_name, 
+            item = :item, 
+            reason = :reason, 
+            breakdown = :breakdown, 
+            solution = :solution
+            WHERE id = :cfl_id
+        """)
+
+        conn.execute(query, {
+            "cfl_id": cfl_id,
+            "partner_name": data['partner_name'],
+            "leader_name": data['leader_name'],
+            "item": data['item'],
+            "reason": data['cfl_reason'],
+            "breakdown": data['breakdown'],
+            "solution": data['solution']
+        })
