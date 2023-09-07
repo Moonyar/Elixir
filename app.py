b9e9c35db6a9dc3b7434d412e00b994ac8d9e060
Moonyar <mahyaresharafi@gmail.com>
2023-09-06 23:24:18 -0700

    Fully Added CFL functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify, request
+from databse import load_cfl, add_cfl
 
 app = Flask(__name__)
 
@@ -7,14 +8,25 @@ app = Flask(__name__)
 def home():
     return render_template('Home.html')
 
+
 @app.route("/cfl")
 def cfl():
     return render_template('cfl.html')
 
+
+@app.route("/cfl/<id>")
+def show_cfl(id):
+    cfl = load_cfl(id)
+    return jsonify(cfl)
+
+
 @app.route("/cfl/submit", methods=['post'])
 def submit_cfl():
     data = request.form
-    return jsonify(data)
+    cfl_id=add_cfl(data)
+    cfl=load_cfl(cfl_id)
+    return render_template('cfl_confirmed.html', cfl=cfl)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- databse.py
+++ databse.py
@@ -16,3 +16,28 @@ engine = create_engine(db_connection_string,
                            }
                        }
                        )
+
+
+def load_cfl(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from cfls where id = :cfl_id"), {"cfl_id": id})
+
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._asdict()
+
+def add_cfl(data):
+    with engine.connect() as conn:
+        query = text(
+            "INSERT INTO cfls (partner, leader, item , reason, breakdown, solution) VALUES (:partner_name, :leader_name, :item, :reason, :breakdown, :solution)")
+
+        conn.execute(query, {"partner_name": data['partner_name'],
+                             "leader_name": data['leader_name'],
+                             "item": data['item'],
+                             "reason": data['cfl_reason'],
+                             "breakdown": data['breakdown'],
+                             "solution": data['solution']})
+        result = conn.execute(text("SELECT LAST_INSERT_ID()"))
+        return result.fetchone()[0]
